2660f6a82135e2f22c599e764ab1db02790e3192
ModEraNoah <klauck02@outlook.com>
2023-12-01 18:36:24 +0100

    day-01 2nd part

--- day-01/day-01.ts
+++ day-01/day-01.ts
@@ -2,11 +2,10 @@ import fs from "fs";
 
 export function trebuchet(path: string) {
     const fileContent = fs.readFileSync(path, "utf8");
-    const allRows = fileContent.split(/r?\n/);
+    const allRows = fileContent.split(/\r?\n/);
 
     let counter = 0;
     for (let i = 0; i < allRows.length - 1; i++) {
-        //console.log(allRows[i]);
         counter += parseInt(getCalibrationValue(allRows[i]));
     }
 
@@ -17,12 +16,36 @@ export function getCalibrationValue(row: string): string {
     let frontPointer: string = "";
     let backPointer: string = "";
 
-    const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const numericNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    const literalNumber = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const literalToNumeric = new Map([
+        ["one", "1"],
+        ["two", "2"],
+        ["three", "3"],
+        ["four", "4"],
+        ["five", "5"],
+        ["six", "6"],
+        ["seven", "7"],
+        ["eight", "8"],
+        ["nine", "9"],
+    ]);
 
+    let firstNumericNumberIndex: number = Infinity;
     for (let i = 0; i < row.length; i++) {
         try {
-            if (numbers.includes(row[i])) {
+            if (numericNumbers.includes(row[i])) {
                 frontPointer = row[i];
+                firstNumericNumberIndex = i;
                 break;
             }
         } catch (e) {
@@ -30,19 +53,41 @@ export function getCalibrationValue(row: string): string {
         }
     }
 
+    let lastNumericNumberIndex: number = -1;
     for (let i = row.length - 1; i >= 0; i--) {
         try {
-            if (numbers.includes(row[i])) {
+            if (numericNumbers.includes(row[i])) {
                 backPointer = row[i];
+                lastNumericNumberIndex = i;
                 break;
             }
         } catch (e) {
             continue;
         }
     }
+
+    let firstLiteralNumberIndex: number = firstNumericNumberIndex;
+    literalNumber.forEach((num) => {
+        let match = row.match(num)?.index;
+        if (match !== null && match! < firstLiteralNumberIndex) {
+            frontPointer = literalToNumeric.get(num)!;
+            //frontPointer = num;
+            firstLiteralNumberIndex = row.match(num)!.index!;
+        }
+    });
+
+    let lastLiteralNumberIndex: number = lastNumericNumberIndex;
+    literalNumber.forEach((num) => {
+        let match = row.lastIndexOf(num);
+        if (match >= 0 && match! > lastLiteralNumberIndex) {
+            backPointer = literalToNumeric.get(num)!;
+            //backPointer = num;
+            lastLiteralNumberIndex = match;
+        }
+    });
 
     return frontPointer + backPointer;
 }
 
-// trebuchet("day-01/day-01-input.txt");
 trebuchet("day-01/day-01-input.txt");
+// trebuchet("day-01/test2-input.txt");
